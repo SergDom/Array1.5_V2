681349fe4f1f56156257de04012065bd86de8c9d
sergeydomanov <nomad-labs@yandex.ru>
2022-08-13 12:37:35 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -43,9 +43,10 @@ public class Main {
 
         System.out.println("Задание 3");
 
-        for ( int i = firstArr.length; firstArr.length <= 2; i--) {
-            System.out.print(firstArr.length -1 + " , ");
-        }
+        for (int i = firstArr.length; i >= 3; i--)
+            if (i<2) { System.out.print(firstArr.length); }
+             else { System.out.print(firstArr.length  + " , ");}
+
 
         System.out.println();
         for (int j = secondArr.length - 1; j > 0; j--) {
@@ -66,12 +67,10 @@ public class Main {
         System.out.println();
         System.out.println("Задание 4");
 
-        for (int i = 0; i <= 3; i++);
-         i = i%2!=0 ;
-        if (firstArr[i]%2!=0)
-         { System.out.print(firstArr[i] + 1 + " ");
-            System.out.print(firstArr[1] + " ");
-            System.out.print(firstArr[2] + 1 + " ");
+      //  int firstArr2 = firstArr[]{1, 2, 3};
+        // for (; firstArr2%2!= 0; firstArr2++);
+
+        // { System.out.print(firstArr2 + " ");
+
         }
     }
-}
