554d53d566d9d5f1f276d0eb8217a1eaa1f84012
sergeydomanov <nomad-labs@yandex.ru>
2022-08-13 22:06:52 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -22,55 +24,66 @@ public class Main {
         firstArr[2] = 3;
 
         for (int i = 0; i < 3; ++i)
-            if (i <= 0) {System.out.print(firstArr[0]);
-            } else { System.out.print(" , " + firstArr[i]);}
+            if (i <= 0) {
+                System.out.print(firstArr[0]);
+            } else {
+                System.out.print(" , " + firstArr[i]);
+            }
 
         System.out.println();
 
-        for ( int j = 0; j < 3; j++)
+        for (int j = 0; j < 3; j++)
             if (j <= 0) {
-            System.out.print(secondArr[0]);}
-        else {System.out.print(" , " + secondArr[j]); }
+                System.out.print(secondArr[0]);
+            } else {
+                System.out.print(" , " + secondArr[j]);
+            }
 
         System.out.println();
 
 
         for (int k = 0; k < 4; k++)
-             if (k <= 0) {System.out.print(thirdArr[0]); }
-              else { System.out.print(" , " + thirdArr[k]); }
+            if (k <= 0) {
+                System.out.print(thirdArr[0]);
+            } else {
+                System.out.print(" , " + thirdArr[k]);
+            }
 
         System.out.println();
 
         System.out.println("Задание 3");
 
-        for (int i = firstArr.length; i >= 3; i--)
-            if (i<2) { System.out.print(firstArr.length); }
-             else { System.out.print(firstArr.length  + " , ");}
+        for (int i =firstArr.length - 1 ; i > 0; i--)
+            System.out.print(firstArr[i] + " , ");
+            if (firstArr.length >= 1) {
+            System.out.print(firstArr[0]); }
 
 
         System.out.println();
         for (int j = secondArr.length - 1; j > 0; j--) {
-            System.out.print(secondArr[j] - 1 + " , ");
+            System.out.print(secondArr[j] + " , ");
+            if (secondArr.length >= 1) {
+                System.out.print(secondArr[0]); }
         }
 
         System.out.println();
 
-        for (
-                int k = thirdArr.length - 1;
-                k > 0; k--) {
-            System.out.print(+thirdArr[k] + " , ");
-        }
-        if (thirdArr.length >= 1) {
+        for (int k = thirdArr.length - 1; k > 0; k--) {
+            System.out.print(thirdArr[k] + " , ");
+        } if (thirdArr.length >= 1) {
             System.out.print(thirdArr[0]);
         }
 
         System.out.println();
+
         System.out.println("Задание 4");
 
-      //  int firstArr2 = firstArr[]{1, 2, 3};
-        // for (; firstArr2%2!= 0; firstArr2++);
-
-        // { System.out.print(firstArr2 + " ");
-
+            for (int i = 0; i < 3; i++)
+             if (i % 2 != 0) {
+                System.out.print(firstArr[i] + " ");
+            } else {
+                System.out.print(firstArr[i] + 1 + " ");
+            }
         }
-    }
+
+}
